4526c9f7bd9e89bb8a46f40e64cde06394c57df1
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2025-04-25 16:14:02 +0200

    auth login route - spring boot test

--- src/main/java/com/openclassromms/api/repository/UserRepository.java
+++ src/main/java/com/openclassromms/api/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.openclassromms.api.repository;
 
 import com.openclassromms.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 }
