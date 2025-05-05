40867e911b6967bd8b6fbdc9354c59219e0ee967
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2025-05-05 15:11:30 +0200

    rentals route - create rental

--- src/main/java/com/openclassromms/api/model/User.java
+++ src/main/java/com/openclassromms/api/model/User.java
@@ -3,15 +3,18 @@ package com.openclassromms.api.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "\"user\"")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
     private String password;
 
     public Long getId() {
